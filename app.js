633ebe499840e1f69724afaa66973e4ef0fdfc87
AdrienneJ123 <jobsadrienne@gmail.com>
2025-10-24 00:46:30 -0500

    Updated JavaScript

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('btn');
   const resultDiv = document.getElementById('result');
-
   const input = document.getElementById('search');
 
   button.addEventListener('click', () => {
@@ -9,13 +8,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const url = `superheroes.php?query=${encodeURIComponent(query)}`;
 
     fetch(url)
-      .then(response => response.text()).then(data => {
+      .then(response => response.text())
+      .then(data => {
         resultDiv.innerHTML = data;
       })
       .catch(error => {
-        
         resultDiv.innerHTML = `<p style="color:red;">Error fetching data: ${error}</p>`;
-
       });
   });
 });
